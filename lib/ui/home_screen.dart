ce5f09aa616e8a4914503e36cdd7f555ba1e73c5
unknown <felipeestevesdepaula@gmail.com>
2025-09-08 14:57:52 -0300

    add statufulWidge

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bank/models/account.dart';
+import 'package:flutter_bank/services/account_service.dart';
 import 'package:flutter_bank/ui/styles/colors.dart';
+import 'package:flutter_bank/ui/widgets/account_widget.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future<List<Account>> _futureGetAll = AccountService().getAll();
+
+  Future<void> refreshGetAll() async {
+    setState(() {
+    _futureGetAll = AccountService().getAll();
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -16,6 +31,39 @@ class HomeScreen extends StatelessWidget {
             }, icon: Icon(Icons.logout), 
            )
          ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: RefreshIndicator (
+          onRefresh: refreshGetAll,
+          child: FutureBuilder(future: _futureGetAll,
+           builder:(context, snapshot) {
+            switch (snapshot.connectionState){ 
+              
+              case ConnectionState.none:
+                return Center(child: CircularProgressIndicator(),);
+          
+              case ConnectionState.waiting:
+                return Center(child: CircularProgressIndicator(),);
+          
+              case ConnectionState.active:
+                return Center(child: CircularProgressIndicator(),);
+          
+              case ConnectionState.done:
+                if (snapshot.data == null || snapshot.data!.isEmpty){
+                  return const Center(child: Text("Nenhuma conta recebida"),);
+                } else {
+                  List<Account> listAccounts = snapshot.data!;
+                  return ListView.builder(
+                    itemCount: listAccounts.length,
+                    itemBuilder: (context, index) {
+                      return AccountWidget(account: listAccounts[index]);
+                    },
+                  );
+                }
+            }
+          }, ),
+        ),
       )
     );
   }
--- lib/ui/styles/colors.dart
+++ lib/ui/styles/colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/widgets.dart';
 class AppColors {
   static const Color orange = Color.fromARGB(255, 255, 177, 21);
   static const Color lightGray = Color(0xfff0f0f0);
+  static const Color lighColor = Color(0xFFFFEDC2);
 }
